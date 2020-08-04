fe0f59856f9f85d435985d0a5bf46e974d6b7c7f
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-08-04 13:28:12 -0400

    Added interactive element (button)

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function() {
       myImage.setAttribute('src','images/business_photo.png');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome, ' + myName;
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
