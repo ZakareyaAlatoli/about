8b22f1dbe86f9bc632ce40c32d4508a198e1fa4e
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-08-16 22:08:38 -0400

    Modified page layout
    -Changed photo dimensions

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/business_photo.png') {
-      myImage.setAttribute('src','images/business_photo_monochrome.png');
+      //myImage.setAttribute('src','images/business_photo_monochrome.png');
     } else {
-      myImage.setAttribute('src','images/business_photo.png');
+      //myImage.setAttribute('src','images/business_photo.png');
     }
 }
 
