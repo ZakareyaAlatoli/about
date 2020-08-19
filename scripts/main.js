416d0e9a4c1be3a2abb04fcc29b6f79a8436ed03
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-08-18 20:26:18 -0400

    Added Twitter profile

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,30 @@
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/business_photo.png') {
-      //myImage.setAttribute('src','images/business_photo_monochrome.png');
-    } else {
-      //myImage.setAttribute('src','images/business_photo.png');
-    }
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/business_photo.png') {
+    //myImage.setAttribute('src','images/business_photo_monochrome.png');
+  } else {
+    //myImage.setAttribute('src','images/business_photo.png');
+  }
 }
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h2');
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome, ' + myName;
-  }
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome, ' + myName;
+}
 
 if(!localStorage.getItem('name')) {
-    //setUserName();
+  //setUserName();
 } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcome, ' + storedName;
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome, ' + storedName;
 }
 
 myButton.onclick = function() {
-    //setUserName();
-  }
+  //setUserName();
+}
